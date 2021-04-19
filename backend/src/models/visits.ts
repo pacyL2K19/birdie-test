72618a79597dd67a25bbbb17eb7cbf4981b86de7
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-04-19 13:48:10 +0200

    Setup the visits model

---
+++ backend/src/models/visits.ts
@@ -0,0 +1,30 @@
+import * as Sequelize from "sequelize";
+import { sequelize } from "../instances/sequelize";
+import { IVisit } from "../types/visit";
+
+enum EventType {
+    MOOD = "mood_observation",
+    HEALTH = "health_check",
+    SLEEP = "sleep"
+}
+
+export interface VisitModel extends Sequelize.Model<VisitModel, IVisit> {
+    id: string,
+    date: string,
+    careGiverId: string,
+    careRecipientId: string,
+    notes: string,
+    eventType: EventType
+}
+
+export const Visit = sequelize.define<VisitModel, IVisit>('visit', {
+    id: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+    },
+    careGiverId: Sequelize.STRING,
+    careRecipientId: Sequelize.STRING,
+    date: Sequelize.DATE,
+    eventType: Sequelize.STRING,
+    note: Sequelize.STRING
+})
