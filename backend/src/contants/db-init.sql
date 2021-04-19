3669fb7e723dd2508ef14d05160ca22b32453f49
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-04-19 12:16:55 +0200

    UPdate the database schema

--- backend/src/contants/db-init.sql
+++ backend/src/contants/db-init.sql
@@ -2,14 +2,25 @@ create database birdie-dev;
 use birdie-dev;
 
 create table users(
-    id int auto_increment primary key,
-    phone int 
-    email text not null,
-    password text not null,
-    createdAt datetime,
-    updatedAt datetime
+    id varchar(36) primary key,
+    names varchar(100) not null,
+    email varchar(255),
+    password varchar(255),
+    role varchar(25),
+    address varchar(255)
 );
 
 create table visits(
-    id int auto_increment primary key,
+    id varchar(36) primary key,
+    date datetime,
+    care_giver_id varchar(36),
+    care_recipient_id varchar(36),
+    note text,
+    event_type varchar
 );
+
+create table care_recipient(
+    id varchar(36),
+    familly_members text,
+    visits text
+)
