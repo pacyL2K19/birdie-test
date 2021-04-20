0be06ff322e76dd98fb2dfc914cad9bdd7265416
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-04-20 13:52:09 +0200

    Design the signup screen

--- front-end/src/components/app/App.tsx
+++ front-end/src/components/app/App.tsx
@@ -3,12 +3,7 @@ import styled, { createGlobalStyle } from 'styled-components';
 import { RootState } from '@App/store/reducers';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
-
-import Title from '@App/components/Title';
-import Logo from '@App/components/Logo';
-import SubTitle from '@App/components/SubTitle';
-
-const LogoUrl = require('../../assets/images/logo-birdie.svg');
+import Signup from '../../pages/Signup';
 
 interface AppProps {
 
@@ -47,9 +42,7 @@ class App extends React.Component<AppProps, AppState> {
       <>
         <GlobalStyle />
         <AppContainer>
-          <Logo src={LogoUrl} />
-          <Title>Welcome to the birdie test</Title>
-          <SubTitle>Best of luck!</SubTitle>
+          <Signup />
         </AppContainer>
       </>
     );
---
+++ front-end/src/pages/Signup/index.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Switch from '@material-ui/core/Switch';
+import Logo from '@App/components/Logo';
+const LogoUrl = require('../../assets/images/logo-birdie.svg');
+
+import { Box, OptionBox } from './styled';
+import { useState } from 'react';
+
+interface Props {
+
+}
+
+const Signup: React.FC = (props: Props) => {
+    const [state, setState] = useState({
+        isFamillyMember: true
+    });
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setState({ ...state, [event.target.name]: event.target.checked });
+    };
+    return (
+        <>
+            <Box>
+                <Logo src={LogoUrl} />
+                <TextField  id="signup-names" label="Names *" fullWidth={true} margin={'normal'} />
+                <TextField  id="signup-email" label="Email *" fullWidth={true} margin={'normal'} />
+                <TextField  id="signup-password" label="Password *" fullWidth={true} margin={'normal'} />
+                <TextField  id="signup-password-confirmation" label="Pasword Confirmation *" fullWidth={true} margin={'normal'} />
+                <TextField  id="signup-phone" label="Phone *" fullWidth={true} margin={'normal'} />
+                <TextField  id="signup-address" label="Address" fullWidth={true} margin={'normal'} />
+                <p>Signup as</p>
+                <OptionBox>
+                    <span>Familly Member</span>
+                    <Switch
+                        checked={state.isFamillyMember}
+                        onChange={handleChange}
+                        name="famillyMember"
+                        inputProps={{ 'aria-label': 'secondary checkbox' }}
+                    />
+                    <span>Care Giver</span>
+                </OptionBox>
+            </Box>
+        </>
+    );
+};
+
+export default Signup;
