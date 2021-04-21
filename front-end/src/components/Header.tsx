6bccb978c459b904e1a153fa4fe25b42fab5859b
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-04-21 16:18:53 +0200

    Minor update

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -3,13 +3,17 @@ import * as React from 'react';
 import { HeaderBox, Left, Right } from './styled';
 import Button from '../components/Button';
 import Avatar from '@material-ui/core/Avatar';
+import Logo from '@App/components/Logo';
+
 const avatar = require('../assets/images/profil.png');
+const LogoUrl = require('../assets/images/logo-birdie.svg');
 
 const Header: React.FC<IUser> = (props: IUser) => {
     return (
         <div>
             <HeaderBox>
                 <Left>
+                    <Logo src={LogoUrl} />
                     <Avatar alt="User" src={avatar}  />
                     <p className="h4">{props.names}</p><br />
                     <p className="h4">{props.email}</p>
